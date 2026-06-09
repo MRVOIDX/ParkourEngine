fb2009fc6a591c866eca7e5167eda03dc9c53734
Oussama Lakhtiri <209028347+MRVOIDX@users.noreply.github.com>
2026-06-10 01:19:56 +0200

    Added Events Listeners

---
+++ src/main/java/dev/maghreb/parkour/events/PlayerQuitListener.java
@@ -0,0 +1,29 @@
+package dev.maghreb.parkour.events;
+
+import dev.maghreb.parkour.managers.RunManager;
+import dev.maghreb.parkour.models.ActiveRun;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerQuitEvent;
+
+/**
+ * Ensures active run state is cleaned up when a player disconnects.
+ */
+public class PlayerQuitListener implements Listener {
+
+    private final RunManager runManager;
+
+    public PlayerQuitListener(RunManager runManager) {
+        this.runManager = runManager;
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR)
+    public void onPlayerQuit(PlayerQuitEvent event) {
+        Player player = event.getPlayer();
+        if (runManager.isRunning(player.getUniqueId())) {
+            runManager.invalidateRun(player, ActiveRun.InvalidationReason.QUIT);
+        }
+    }
+}
